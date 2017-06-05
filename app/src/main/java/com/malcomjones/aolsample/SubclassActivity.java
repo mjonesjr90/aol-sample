9ea6d7aadea9bd897d2d5629f06e942a9b2fcdca
Malcom Jones <malcom.jones@teamaol.com>
2017-06-05 15:24:57 -0400

    added user info and app info

--- app/src/main/java/com/malcomjones/aolsample/SubclassActivity.java
+++ app/src/main/java/com/malcomjones/aolsample/SubclassActivity.java
@@ -1,13 +1,38 @@
 package com.malcomjones.aolsample;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.app.Application;
+import android.util.Log;
 
-public class SubclassActivity extends AppCompatActivity {
+import com.millennialmedia.AppInfo;
+import com.millennialmedia.MMSDK;
+import com.millennialmedia.UserData;
+import com.millennialmedia.UserData.Gender;
+
+public class SubclassActivity extends Application {
+
+    private static final String TAG = "SUBCLASS ACTIVITY";
+    private static final String SITE_ID = "9993e2e5945d4bbca5f799177f7cacf5";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_subclass);
+    public void onCreate() {
+        super.onCreate();
+
+        try{
+            MMSDK.initialize(this);
+
+            //Set Site ID
+            AppInfo appInfo = new AppInfo();
+            appInfo.setSiteId(SITE_ID);
+            MMSDK.setAppInfo(appInfo);
+
+            //Set User Information
+            UserData userData = new UserData().
+                    setAge(26).
+                    setGender(Gender.MALE);
+            MMSDK.setUserData(userData);
+        } catch (Exception e){
+            Log.e(TAG, "SDK didn't initialize", e);
+        }
+
     }
 }
